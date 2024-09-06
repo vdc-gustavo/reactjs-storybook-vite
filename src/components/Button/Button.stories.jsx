a28a7ab5f8215a76e5194f16cfafe3193c2d52a8
Gustavo Valenga de Campos <88239833+vdc-gustavo@users.noreply.github.com>
2024-09-05 22:30:49 -0300

    starting storybook

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,11 +1,19 @@
-import  Button  from './Button';
+import Button from "./Button";
 
 export default {
   component: Button,
 };
 
 export const Primary = {
+  name: "Primary",
   args: {
-    label: "Clique aqui",
+    label: "Button",
+  },
+};
+
+export const Secondary = {
+  args: {
+    ...Primary.args,
+    label: "Secondary",
   },
 };
